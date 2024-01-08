b805600c3b2cdc8cc40477714cde2f2d85eb3043
williamvnobrega <williamvnobrega@gmail.com>
2024-01-08 16:03:19 -0300

    project created

---
+++ index.js
@@ -0,0 +1,7 @@
+const inputFieldEl = document.getElementById("input-field");
+const addBtn = document.getElementById("add-button");
+
+addBtn.addEventListener("click",()=>{
+    let inputValue = inputFieldEl.value
+    console.log(inputValue)
+})
