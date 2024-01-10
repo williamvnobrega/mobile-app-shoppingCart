bbb89d4098fdcfc548126f2960e3605a43d55ffe
williamvnobrega <williamvnobrega@gmail.com>
2024-01-10 14:34:40 -0300

    feature: set up and connect firebase as database

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+
+
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase,ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+
+const appSettings = {
+    databaseURL: "https://realtime-database-1b8b6-default-rtdb.firebaseio.com/"
+}
+const app = initializeApp(appSettings)
+const database = getDatabase(app);
+const shoppingListInDB = ref(database,"shoppingList")
+
 const inputFieldEl = document.getElementById("input-field");
 const addBtn = document.getElementById("add-button");
 
 addBtn.addEventListener("click",()=>{
     let inputValue = inputFieldEl.value
+    push(shoppingListInDB,inputValue)
     console.log(inputValue)
 })
+
+
