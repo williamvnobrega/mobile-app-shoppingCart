743727239da146bc5138df14e844825f27bc2182
williamvnobrega <williamvnobrega@gmail.com>
2024-01-12 14:52:42 -0300

    fix: correct initialization of const shoppingListEl

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import {getDatabase,ref, push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-
 const appSettings = {
     databaseURL: "https://realtime-database-1b8b6-default-rtdb.firebaseio.com/"
 }
@@ -11,7 +10,7 @@ const shoppingListInDB = ref(database,"shoppingList")
 
 const inputFieldEl = document.getElementById("input-field");
 const addBtn = document.getElementById("add-button");
-const shoppingListEl = document.getElementById
+const shoppingListEl = document.getElementById('shopping-list');
 
 
 addBtn.addEventListener("click",()=>{
@@ -24,6 +23,7 @@ addBtn.addEventListener("click",()=>{
 onValue(shoppingListInDB,function(snapshot){
     let itemsArray = Object.values(snapshot.val())
     clearShoppingListEl()
+
     for(let i = 0; i<itemsArray.length;i++){
     appendItemToShoppingListEl(itemsArray[i])
     }
